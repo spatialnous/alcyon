e091d119dc7ce6e91e05fea7f3f7dae30d3ef0a9
Petros Koutsolampros <commits@pklampros.io>
2024-06-03 12:22:02 +0300

    Add some scripts for building in containers

---
+++ scripts/buildContainer.sh
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: Petros Koutsolampros
+# SPDX-License-Identifier: GPL-3.0-only
+
+#!/bin/bash
+
+# install container
+podman build -t alcyon-debug-builder \
+-<< EOF
+FROM docker.io/wch1/r-debug:latest
+RUN apt-get update && apt-get upgrade -y && apt-get install -y libudunits2-dev libgdal-dev cmake
+RUN RDsan -e 'install.packages(c("sp", "sf", "testthat"))'
+RUN RDsan -e 'install.packages(c("igraph"))'
+EOF
+
---
+++ scripts/runContainerInteractive.sh
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: Petros Koutsolampros
+# SPDX-License-Identifier: GPL-3.0-only
+
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+podman run -it --volume=../:/alcyon/ \
+    --workdir=/alcyon \
+     alcyon-debug-builder:latest \
+     /bin/bash
+
+
+# To test inside the container:
+
+# RDsan -e 'install.packages("/alcyon", repos=NULL)'
+# RDsan -e 'testthat::test_file("/alcyon/tests/testthat/test-isovist.R", package="alcyon")'
+# RDsan -e 'testthat::test_file("/alcyon/tests/testthat/test-agentanalysis.R", package="alcyon")'
+# RDsan -e 'testthat::test_file("/alcyon/tests/testthat/test-MetaGraph.R", package="alcyon")'
+# RDsan -e 'testthat::test_dir("/alcyon/tests")'
+
+
+# Or, inside RDsan inside the container:
+
+# install.packages("/alcyon", repos=NULL)
+# testthat::test_file("/alcyon/tests/testthat/test-isovist.R", package="alcyon")
+# testthat::test_file("/alcyon/tests/testthat/test-agentanalysis.R", package="alcyon")
+# testthat::test_file("/alcyon/tests/testthat/test-MetaGraph.R", package="alcyon")
+# testthat::test_dir("/alcyon/tests")
